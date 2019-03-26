acc9c5cad79cbaac3ddce910eeedb48ea45b5489
dungnv52tk <dungnv52tk@gmail.com>
2019-03-26 16:09:47 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let x, S, C;
+x =  prompt("Nhap vao ban kinh hinh tron: 0");
+let r = parseInt(x);
+C = 2*r*3.14;
+S = r*r*3.14;
+alert("Dien tich hinh tron la: " + S);
+alert("Chu vi hinh tron la: " + C);
